c31b2956ead78b68275b376fa4a208b19aa8319b
Gregg Trunnell <gggtrunnell@gmail.com>
2024-09-30 14:18:03 -0500

    if else prac

--- variables.js
+++ variables.js
@@ -1,4 +1,5 @@
 
+/*
 let catName= 'Rue';
 let catAge= 3;
 console.log( 'the name of the cat is', catName);
@@ -16,3 +17,13 @@ catAge= 14;
 
 sentance= 'later on, a different cat is '+ catAge +' old and is named '+ catName ;
 console.log(sentance);
+*/
+
+let thing1= 'gold';
+
+if (thing1== 'tiny ball'){
+    console.log('it is a basketball');
+    } 
+else if(thing1=='elephant'){console.log('it is an elephant');
+}
+else {console.log('not ball or elephant');}
