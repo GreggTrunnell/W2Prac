194d737db701f4c339d4d8abc7a121c7bcc750e3
Gregg Trunnell <gggtrunnell@gmail.com>
2024-09-30 14:02:44 -0500

    first

---
+++ variables.js
@@ -0,0 +1,18 @@
+
+let catName= 'Rue';
+let catAge= 3;
+console.log( 'the name of the cat is', catName);
+console.log( 'the age of my cat is:', catAge);
+
+let sentance= 'this is text about the cat named ' + catName;
+
+console.log( sentance);
+
+sentance= 'the cat is ' + catAge + ' years old';
+console.log( sentance );
+
+catName= 'sophia';
+catAge= 14;
+
+sentance= 'later on, a different cat is '+ catAge +' old and is named '+ catName ;
+console.log(sentance);
